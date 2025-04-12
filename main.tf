6af6f6302fdc71a6826b44e4e043b99976543a30
Dipesh Agarwal <147990547+dipeshagarwaaal@users.noreply.github.com>
2025-04-12 11:22:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "azurerm_service_plan" "Web_plan" {
   sku_name = "B1"
 
 }
-resource "azurerm_app_service" "web_app" {
+resource "azurerm_app_service" "web_app"{
   name                = "AgarwalReactWebAppfinal"
   location            = azurerm_resource_group.web_rg.location
   resource_group_name = azurerm_resource_group.web_rg.name
